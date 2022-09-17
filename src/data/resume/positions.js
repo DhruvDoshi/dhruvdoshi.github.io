720f3341ac1b80d0b11b9025d376db86174533b1
AniPetrosyan <50109999+AniPetrosyan@users.noreply.github.com>
2022-09-17 17:06:01 +0400

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -1,13 +1,48 @@
 const positions = [
+
+  {
+    company: 'DataPoint Armenia - ',
+    position: ' Data Science Fellow',
+    link: 'https://datapoint.am/',
+    daterange: 'Jun 2022 – Aug 2022',
+    points: [
+      "Implemented customer segmentation task through unsupervised learning models such as DB-Scan, K-means, Affinity propagation on big data",
+      'Cooperated with Coinstats executives to share the vision and the mission of the project',
+      'Developed skills further through 20 active learning and 10 hands-on mentorship sessions',
+    ],
+  }, 
+
   {
     company: 'Girlopolis Organization - ',
     position: ' Founder',
     link: 'https://www.facebook.com/HackathonForGirls',
-    daterange: 'December 2020 - Present',
+    daterange: 'Dec 2020 – Aug 2022',
     points: [
-      'Exposing girls to professional opportunities in STEM and business fields by organizing hackathons',
-      'Encouraging girls to pursue careers in STEM by conducting webinars',
-      'Organized 5 meetups and 2 hackathons (with over 180 participants and 20 volunteers); won 5 grants; Girlopolis won Social Impact Award Armenia',
+      'Mission: Encouraging Armenian girls to pursue careers in STEM and business fields',
+      "Organized 3 hackathons (with over 480 participants), 5 meetups, won 5 grants, and raised $4500",
+      'Brought together of 10 individuals team of prior executives and 20 volunteers at the age of 16',
+    ],
+  },
+
+  {
+    company: 'Innovative Solutions and Technologies Center (ISTC) - ',
+    position: ' Accelerator program assistant',
+    link: 'https://www.istc.am/',
+    daterange: 'Aug 2020 – Nov 2020',
+    points: [
+      'Managed the logistics of the accelerator #5 program and supported 20 startups',
+      "Organized 2 community meetups hosting 400 technology specialists to help strengthen the technology sector",
+    ],
+  },
+
+
+  {
+    company: '"All In" group - ',
+    position: ' BackEnd developer intern',
+    link: '',
+    daterange: 'Oct 2020 – Jan 2021',
+    points: [
+      'Implemented 4 back-end tasks with Django and Flask for the website and created Telegram chatbots',
     ],
   },
 ];
